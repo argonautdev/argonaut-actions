aaaa01df84c655a99fab72317f8f79b3aaa78b48
Surya Oruganti <suryaoruganti@gmail.com>
2020-09-21 14:58:30 +0530

    playing around

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Entrypoint activated"
 echo "Hello $1"
 time=$(date)
-echo "$time"
+echo "time in script is fucked: $time"
 echo "::set-output name=time-now::$time"
 # echo "::set-env name=time::$time"
