828c21a87b9a66536b5fee56f487ee5bfb1635b9
Surya Oruganti <suryaoruganti@gmail.com>
2020-09-21 16:19:31 +0530

    using docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 
-
 NAME=$1
 CWD=`pwd`
-echo "Entrypoint activated"
 echo "Heave ho $NAME"
-chmod +x $CWD/goodbye.sh
 time=$(date)
 echo "::set-output name=time-now::$time"
+
+chmod +x $CWD/goodbye.sh
 sh goodbye.sh
