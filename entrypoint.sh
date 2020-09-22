88743ca5347b42d15dddea640b91355b20953b86
Surya Oruganti <suryaoruganti@gmail.com>
2020-09-22 13:40:36 +0530

    half setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,13 +28,20 @@ curl -s "https://raw.githubusercontent.com/\
 kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 
 # SETUP eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C ./
 
 # SETUP aws configure
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
+pwd
+ls
 # TODO: Incorporate this into argonaut templates https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 
 # SETUP argonaut
+curl -s "https://raw.githubusercontent.com/argonautdev/argonaut-actions/master/argonaut"
+
+cd ../
+argonaut build 
+argonaut apply
