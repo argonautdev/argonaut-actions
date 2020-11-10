ff9211bcf1ab50a6444ee0635da498684607cc3f
Surya Oruganti <suryaoruganti@gmail.com>
2020-11-10 13:58:24 +0530

    something

--- gitlab.sh
+++ gitlab.sh
@@ -91,12 +91,13 @@ env
 
 # Create ArgoCD app release
 echo "Creating ArgoCD app release"
-echo "Adding repo: argocd repo add https://gitlab.com/$CI_PROJECT_PATH.git --username $GIT_USER --password $GIT_PUSH_TOKEN --upsert
+echo "Adding repo: argocd repo add $CI_REPOSITORY_URL --upsert
 "
-argocd repo add https://gitlab.com/$CI_PROJECT_PATH.git --username $GIT_USER --password wddNV54AT1kRnjWETNuu --upsert
+# argocd repo add https://gitlab.com/$CI_PROJECT_PATH.git --username $GIT_USER --password $GIT_PUSH_TOKEN --upsert
+argocd repo add $CI_REPOSITORY_URL --upsert
 echo "Creating argo app"
-echo "$APP_NAME-release --repo https://gitlab.com/$CI_PROJECT_PATH.git --path argonaut-configs --dest-server $CLUSTER_SERVER --dest-namespace $ENV_NAME --auto-prune --sync-policy automated --upsert"
-argocd app create "$APP_NAME-release" --repo https://gitlab.com/$CI_PROJECT_PATH.git --path argonaut-configs --dest-server $CLUSTER_SERVER --dest-namespace $ENV_NAME --auto-prune --sync-policy automated --upsert
+echo "$APP_NAME-release --repo $CI_REPOSITORY_URL --path argonaut-configs --dest-server $CLUSTER_SERVER --dest-namespace $ENV_NAME --auto-prune --sync-policy automated --upsert"
+argocd app create "$APP_NAME-release" --repo $CI_REPOSITORY_URL --path argonaut-configs --dest-server $CLUSTER_SERVER --dest-namespace $ENV_NAME --auto-prune --sync-policy automated --upsert
 echo "Syncing argo app"
 argocd app sync "$APP_NAME-release"
 
