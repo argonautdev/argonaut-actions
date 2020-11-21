46c263ba86669cc810bb46519fc9da3cd456ef08
Surya Oruganti <suryaoruganti@gmail.com>
2020-11-21 15:12:25 +0530

    changing gitlab.sh to tools.tritonhq.io reference

--- gitlab.sh
+++ gitlab.sh
@@ -59,7 +59,7 @@ aws eks --region us-east-2 update-kubeconfig --name $CLUSTER_NAME
 # Install ArgoCD CLI
 echo "Installing ArgoCD CLI"
 
-export ARGOCD_SERVER="argonaut.tritonhq.io"
+export ARGOCD_SERVER="tools.tritonhq.io"
 # export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output .status.loadBalancer.ingress[0].hostname`
 # export ARGO_PWD=`kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2`
 export ARGO_PWD="1234567890"
