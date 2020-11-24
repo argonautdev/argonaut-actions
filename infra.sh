e911c51d74c81bbe75d67070fead4b9d2dda0f1c
Surya Oruganti <suryaoruganti@gmail.com>
2020-11-24 12:37:52 +0530

    Changed most installs to be self managed and moved to helm. infra.sh as a ton of changes

--- infra.sh
+++ infra.sh
@@ -1,113 +1,89 @@
 #!/bin/sh -l
 
-cd argonaut-configs
+ENV_NAME=dev
+TOOLS_NS=tools
+ARGONAUT_WS=argonaut-configs
+SETUP_CONFIGS=_onetimesetup
+
+cd $ARGONAUT_WS
+
+## install helm
+# curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+## Install kubectl, eksctl, copy istio binary
 
 # Setup EKS cluster
-eksctl create cluster -f _onetimesetup/awsclusterconfig.yaml
+eksctl create cluster -f $SETUP_CONFIGS/awsclusterconfig.yaml
 
 # If create cluster fails after control plane and before nodegroup setup, kubeconfig is not updated
 # aws eks --region us-east-2 update-kubeconfig --name "shadow"
 
-## install helm
-# curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-kubectl create namespace tools
+
+# Cluster configurations
+# Create the environment
+kubectl create namespace $ENV_NAME
+kubectl label namespace $ENV_NAME istio-injection=enabled
+kubectl create namespace $TOOLS_NS
+
+# Setup Storage Class to be used by applications
+kubectl apply -f $SETUP_CONFIGS/storage-class.yaml  # Independent of namespace
+# Unset gp2 as default storage class since we are defining our own
+kubectl patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 
 # Install ISTIO and the observability stack
 curl -L https://istio.io/downloadIstio | sh -
 mv istio-1.8.0/bin/istioctl .
 chmod a+x istioctl
-./istioctl install --set profile=default -y -f _onetimesetup/istio-setup.yaml
-
-# Checking if timeout helps with kiali monitoring dashboard creation
-kubectl apply -f _onetimesetup/addons/ -n istio-system
-# Retry because first time doesn't create all entities
-kubectl apply -f _onetimesetup/addons/ -n istio-system
-
-
-# Install cert manager using helm
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm upgrade --install cert-manager jetstack/cert-manager -n tools -f _onetimesetup/helm-values/cert-manager.yaml 
-
-# # Install cert-manager without helm
-# kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.4/cert-manager.yaml
-
-# Issuer in istio-system namespace
-# Needs to wait for certmanager pods to be ready
-sleep 20s
-kubectl apply -f _onetimesetup/certificate-issuer.yaml # Cluster scoped, not namespace specific
-kubectl -n istio-system apply -f _onetimesetup/certificate.yaml   # Needs wait if secret needs creation
-
-# Setup Storage Class to be used by applications
-kubectl -n tools apply -f _onetimesetup/storage-class.yaml  # Independent of namespace
-# Unset gp2 as default storage class since we are defining our own
-kubectl patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-
-
-# Install argocd
-# kubectl create namespace argocd
-# OPTIONAL istio injection
-# kubectl label namespace tools istio-injection=enabled
-
-# kubectl apply -n tools -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
-# kubectl patch deployment argocd-server --type json -p='[ { "op": "replace", "path":"/spec/template/spec/containers/0/command","value": ["argocd-server","--staticassets","/shared/app","--insecure"] }]' -n argocd
-
-# # # ArgoCD password reset to 1234567890
-# kubectl -n tools patch secret argocd-secret \
-#   -p '{"stringData": {
-#     "admin.password": "$2a$10$Vnr.0q6Gv/rpMouOaF9dPO4TBgPsflxFQHiOZkKckoTvFiwwwsLYO",
-#     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
-#   }}'
-
-# ArgoCd Helm Install
-helm repo add argo https://argoproj.github.io/argo-helm
-helm upgrade --install argocd argo/argo-cd -n tools -f _onetimesetup/helm-values/argocd.yaml 
-
-
-
-# SETUP INGRESS
-kubectl -n tools apply -f _onetimesetup/ingress.yaml
-
-# Create the environment
-kubectl create namespace dev
-kubectl label namespace dev istio-injection=enabled
-
-# # TODO
-# External service provisioning - self service
-# Persistent volumes
-# Clickhouse
-# Hasura
+./istioctl install --set profile=default -y -f $SETUP_CONFIGS/istio-setup.yaml
 
 rm -rf istio-1.8.0/
 rm istioctl
 
-# # HELM INSTALLS
-## INSTALL HELM FIRST - TODO
+# HELM Charts - repo add
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo add kiali https://kiali.org/helm-charts
+helm repo add jetstack https://charts.jetstack.io
+helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add loki https://grafana.github.io/loki/charts
-# ## Grafana
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm upgrade --install grafana grafana/grafana -n tools -f _onetimesetup/helm-values/grafana.yaml 
 
-# ## Loki
-helm upgrade --install loki loki/loki -n tools -f _onetimesetup/helm-values/loki.yaml 
+# HELM Installs
+# Prometheus server and node exporter
+helm upgrade --install prometheus prometheus-community/prometheus -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/prometheus.yaml 
+# Fluent-Bit
+helm upgrade --install fluent-bit loki/fluent-bit -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/fluent-bit-loki.yaml
+# Grafana
+helm upgrade --install grafana grafana/grafana -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/grafana.yaml 
+# Loki
+helm upgrade --install loki loki/loki -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/loki.yaml 
+# Cert-Manager
+helm upgrade --install cert-manager jetstack/cert-manager -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/cert-manager.yaml 
+# ArgoCd
+helm upgrade --install argocd argo/argo-cd -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/argocd.yaml 
 
-# ## FluentBit - already added the charts for loki
-helm upgrade --install fluent-bit loki/fluent-bit -n tools -f _onetimesetup/helm-values/fluent-bit-loki.yaml 
+# Install Jaeger - without persistence or a data store - TODO
+# Note: The jaeger UI doesn't work because the base-path is to be changed to /jaeger
+helm upgrade --install -n $TOOLS_NS jaeger jaegertracing/jaeger-operator --set jaeger.create=true --set fullnameOverride=jaeger 
+# helm upgrade --install -n $TOOLS_NS jaeger jaegertracing/jaeger -f $SETUP_CONFIGS/helm-values/jaeger.yaml
+# Install Jaeger
+# kubectl apply -f $SETUP_CONFIGS/addons/jaeger.yaml -n $TOOLS_NS
 
-# # FluentBit official install 
-# helm repo add fluent https://fluent.github.io/helm-charts
-# helm repo add elastic https://helm.elastic.co
-# helm repo update
+# Install Kiali
+# helm upgrade --install kiali-operator kiali/kiali-operator -n $TOOLS_NS
+# kubectl -n $TOOLS_NS apply -f $SETUP_CONFIGS/kiali_cr.yaml
+# TODO: This is temporary until the helm chart is bumped up to v1.27and beyond
+helm upgrade --install kiali-operator kiali/kiali-operator -n $TOOLS_NS -f $SETUP_CONFIGS/helm-values/kiali-operator-service.yaml
 
-# helm upgrade --install fluent-bit fluent/fluent-bit -n tools -f _onetimesetup/helm-values/fluent-bit.yaml 
-# helm upgrade --install elasticsearch elastic/elasticsearch -n tools -f _onetimesetup/helm-values/elasticsearch.yaml 
-# helm upgrade --install kibana elastic/kibana -n tools -f _onetimesetup/helm-values/kibana.yaml 
-# helm upgrade --install apm-server elastic/apm-server -n tools -f _onetimesetup/helm-values/elastic-apm.yaml 
+# Setup Cluster Certificate Issuer in istio-system namespace. Needs to wait for certmanager pods to be ready
+kubectl apply -f $SETUP_CONFIGS/certificate-issuer.yaml # Cluster scoped, not namespace specific
+# Setup Certificate in istio-system namespace.
+# Do not forget to add DNS for new domains. Needs wait for new domains.
+kubectl -n istio-system apply -f $SETUP_CONFIGS/certificate.yaml
+# Setup Ingress
+kubectl -n $TOOLS_NS apply -f $SETUP_CONFIGS/ingress.yaml
 
 # Print hostname for DNS
-echo "ADD THIS loadbalancer ip TO YOUR DNS at aws.tritonhq.io AND argonaut.tritonhq.io"
+echo "ADD THIS loadbalancer ip TO YOUR DNS at aws.tritonhq.io AND tools.tritonhq.io AND app.tritonhq.io"
 kubectl get -n istio-system services | grep ingress
